af358df3b714bf926d84a2bac2a4cf8a631e8146
mohbadar <mohammadbadarhashimi@gmail.com>
2019-12-12 10:37:20 +0430

    init

---
+++ src/test/java/af/asr/phoenixqrcodegenerator/PhoenixQrcodegeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package af.asr.phoenixqrcodegenerator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhoenixQrcodegeneratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
