bfd3915240be947d38babea30d83093e1af064c4
mohbadar <mohammadbadarhashimi@gmail.com>
2019-12-12 10:51:12 +0430

    #40

---
+++ src/main/java/af/asr/qrcodegenerator/util/QrcodegeneratorUtils.java
@@ -0,0 +1,29 @@
+package af.asr.qrcodegenerator.util;
+
+public class QrcodegeneratorUtils {
+    /**
+     * Constructor for this class
+     */
+    private QrcodegeneratorUtils() {
+
+    }
+
+    /**
+     * Verify the input send by user
+     *
+     * @param data    data send by user
+     * @param version {@link QrVersion} send by user
+     */
+    public static void verifyInput(String data, QrVersion version) {
+        if (data == null) {
+            throw new NullPointerException(QrcodeExceptionConstants.INVALID_INPUT_DATA_NULL.getErrorCode(),
+                    QrcodeExceptionConstants.INVALID_INPUT_DATA_NULL.getErrorMessage());
+        } else if (data.trim().isEmpty()) {
+            throw new InvalidInputException(QrcodeExceptionConstants.INVALID_INPUT_DATA_EMPTY.getErrorCode(),
+                    QrcodeExceptionConstants.INVALID_INPUT_DATA_EMPTY.getErrorMessage());
+        } else if (version == null) {
+            throw new NullPointerException(QrcodeExceptionConstants.INVALID_INPUT_VERSION.getErrorCode(),
+                    QrcodeExceptionConstants.INVALID_INPUT_VERSION.getErrorMessage());
+        }
+    }
+}
